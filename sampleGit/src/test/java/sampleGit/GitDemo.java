99550ec3193b348edee4864fb4bc23b115ce2b8c
Silpa paul <silpapul05@gmail.com>
2025-03-13 14:48:00 +0530

    push

--- sampleGit/src/test/java/sampleGit/GitDemo.java
+++ sampleGit/src/test/java/sampleGit/GitDemo.java
@@ -5,6 +5,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("welcome to the world");
 		System.out.println("India");
+		System.out.println("odisha");
 
 	}
 
